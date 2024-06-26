9aeaa9e09fbfe260b06051dcda4940587b8fe76e
Michael Lange <dingoeatingfuzz@gmail.com>
2024-06-26 16:01:44 -0700

    Maybe don't leave everything commented out on main

--- get-data.sh
+++ get-data.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-#git-data -s '-vagrant' https://github.com/hashicorp/vagrant
-#git-data -s '-vagrant-share' https://github.com/hashicorp/vagrant-share
-#git-data -s '-vagrant-vsphere' https://github.com/nsidc/vagrant-vsphere
-#git-data -s '-vagrant-vmware-desktop' https://github.com/hashicorp/vagrant-vmware-desktop
-#git-data -s '-vagrant-libvirt' https://github.com/vagrant-libvirt/vagrant-libvirt
-#git-data -s '-vagrant-vbguest' https://github.com/dotless-de/vagrant-vbguest
-#git-data -s '-vagrant-qemu' https://github.com/ppggff/vagrant-qemu
-#git-data -s '-vagrant-lxc' https://github.com/fgrehm/vagrant-lxc
+git-data -s '-vagrant' https://github.com/hashicorp/vagrant
+git-data -s '-vagrant-share' https://github.com/hashicorp/vagrant-share
+git-data -s '-vagrant-vsphere' https://github.com/nsidc/vagrant-vsphere
+git-data -s '-vagrant-vmware-desktop' https://github.com/hashicorp/vagrant-vmware-desktop
+git-data -s '-vagrant-libvirt' https://github.com/vagrant-libvirt/vagrant-libvirt
+git-data -s '-vagrant-vbguest' https://github.com/dotless-de/vagrant-vbguest
+git-data -s '-vagrant-qemu' https://github.com/ppggff/vagrant-qemu
+git-data -s '-vagrant-lxc' https://github.com/fgrehm/vagrant-lxc
 git-data -s '-vagrant-installers' https://github.com/hashicorp/vagrant-installers
